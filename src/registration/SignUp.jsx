1393ce117809c2a4c52bc43c4b321734cb59ee4a
Ebraam Sobhy <Ebram@gmail.com>
2025-01-21 17:52:41 +0200

    first commit

--- src/registration/SignUp.jsx
+++ src/registration/SignUp.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { validateEmail } from "../utils/helper";
 import axios from "axios";
 import { toast } from "react-toastify";
-import './Signup.css';
+import './signup.css'
 
 const Signup = () => {
   const [name, setName] = useState("");
